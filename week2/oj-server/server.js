98fd5c4e8f3bdb820e285100ae4fa1d488384c62
shuojianye <shuojianye@gmail.com>
2017-12-31 08:23:02 -0800

    asd

--- week2/oj-server/server.js
+++ week2/oj-server/server.js
@@ -8,10 +8,10 @@ mongoose.connect('mongodb://ysj:yyssjj@ds127936.mlab.com:27936/cs503sy');
 
 app.use('/',indexRouter);
 app.use('/api/v1',restRouter);
-app.use(express.static(path.join(__dirname,'../../public/')));
+app.use(express.static(path.join(__dirname,'../public/')));
 
 app.use((req,res)=> {
-    res.sendFile('index.html',{ root:path.join(__dirname,'../../public/')})
+    res.sendFile('index.html',{ root:path.join(__dirname,'../public/')})
 })
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
