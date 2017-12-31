399eaa17aa1b2830ddc8c02125b9fc2db7465db2
shuojianye <shuojianye@gmail.com>
2017-12-30 20:10:08 -0800

    asd

--- week2/oj-server/server.js
+++ week2/oj-server/server.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const restRouter = require('./routes/rest');
 const indexRouter = require('./routes/index');
 const mongoose  = require('mongoose');
-mongoose.connect('mongodb://shuojian:shuojian@ds127936.mlab.com:27936/cs503sy');
+mongoose.connect('mongodb://ysj:yyssjj@ds127936.mlab.com:27936/cs503sy');
 
 app.use('/',indexRouter);
 app.use('/api/v1',restRouter);
